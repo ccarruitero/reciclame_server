cb253ac555ca796f5fa1d86c1d839f05167f3843
Cesar Carruitero <cesar@mozilla.pe>
2012-10-25 17:52:05 -0500

    initial scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reciclame::Application.config.secret_token = 'c84f5a2d2a3b53c68c40d95c380bf439b20ca1b987d5a8240c87193db95eacebdd574c3352faaa7f3f1d2dacc39cf9e1fa97361269dcb60f62763cc0ec5fa757'

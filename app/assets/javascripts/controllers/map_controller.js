905364204a716a0fc0a220d5c76c0fefeae2b92b
Cesar Carruitero <cesar@mozilla.pe>
2013-05-19 17:55:48 -0500

    map

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -1,19 +1,17 @@
 Reciclame.MapController = Ember.ObjectController.extend({
-  lat: null,
-  lng: null,
   init: function(){
-    var map =  L.mapbox.map('map','ccarruitero.map-d86ft9dj');
-    map.setView([37.9, -77], 5);
-    /*if("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        var lat = position.coords.latitude,
-            lng = position.coords.longitude;
-        map.setView([lat, lng], 15);
-        var marker = L.marker([lat, lng]).addTo(map);
-      });
-    } else {
-      map.setView([37.9, -77], 5);
-    }*/
+    setTimeout(function(){
+      var map =  L.mapbox.map('map','ccarruitero.map-d86ft9dj');
+      if("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          var lat = position.coords.latitude,
+              lng = position.coords.longitude;
+          map.setView([lat, lng], 15);
+          var marker = L.marker([lat, lng]).addTo(map);
+        });
+      } else {
+        map.setView([37.9, -77], 5);
+      }
+    }, 2000);
   }
-  
 });

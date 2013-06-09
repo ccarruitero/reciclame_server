03cc2d899c38c4c9854c61ba7bc4e4d08ec8d570
Cesar Carruitero <cesar@mozilla.pe>
2013-06-09 17:23:42 -0500

    add controllers

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -1,17 +1,51 @@
 Reciclame.MapController = Ember.ObjectController.extend({
+  needs: ['places'],
+
   init: function(){
+
+    var that = this;
+
     setTimeout(function(){
-      var map =  L.mapbox.map('map','ccarruitero.map-d86ft9dj');
+      window.map =  L.mapbox.map('map','ccarruitero.map-d86ft9dj');
       if("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position){
           var lat = position.coords.latitude,
               lng = position.coords.longitude;
-          map.setView([lat, lng], 15);
+          map.setView([lat, lng], 13);
           var marker = L.marker([lat, lng]).addTo(map);
         });
       } else {
-        map.setView([37.9, -77], 5);
+        map.setView([37.9, -77], 13);
       }
+
+      $.getJSON('http://localhost:3000/places.json', function(data){
+        var places = data.places;
+
+        places.map(function(place){
+          that.addMarker(place);
+        });
+      });
     }, 2000);
+  },
+
+  addMarker: function(place){
+    var lat = place.lat;
+    var lng = place.lng;
+
+    L.mapbox.markerLayer({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+        },
+        properties: {
+            'marker-color': '#000',
+            'marker-symbol': 'waste-basket',
+            'marker-size': 'medium',
+            title: place.name,
+            address: place.address
+        }
+    }).addTo(map);
   }
+  
 });
--- app/assets/javascripts/controllers/place_controller.js
+++ app/assets/javascripts/controllers/place_controller.js
@@ -1,6 +1,13 @@
 Reciclame.PlaceController = Ember.ObjectController.extend({
   isEditing: false,
 
+  init: function(){
+    //var lat = this.model.get('lat');
+    //var lng = this.model.get('lng');
+    //map.setView([lat, lng],17);
+    console.log(this.get('model.lat'));
+  },
+
   doneEditing: function(){
     this.set('isEditing', false);
     this.get('store').commit();

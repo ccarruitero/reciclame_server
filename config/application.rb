85995e7765152e291de2ee99f904743087095698
Cesar Carruitero <cesar@mozilla.pe>
2013-10-27 23:01:58 -0500

    fix assets in heroku

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module Reciclame
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.serve_static_assets = true
   end
 end

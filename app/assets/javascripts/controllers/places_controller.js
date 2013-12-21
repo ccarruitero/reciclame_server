37c1f39fc8c29442bcd1f0ed637e24257da67b07
Cesar Carruitero <cesar@mozilla.pe>
2013-12-21 15:28:02 -0500

    select box for mobile

--- app/assets/javascripts/controllers/places_controller.js
+++ app/assets/javascripts/controllers/places_controller.js
@@ -1,3 +1,8 @@
 Reciclame.PlacesController = Ember.ArrayController.extend({
+  selectedPlace: null,
 
+  selectedPlaceChange: function(){
+    var selected = this.get('selectedPlace');
+    this.transitionToRoute('place', selected);
+  }.observes('selectedPlace')
 });

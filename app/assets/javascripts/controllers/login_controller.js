924da59ebba62915dce879225c69fda141d6b0b0
Cesar Carruitero <cesar@mozilla.pe>
2014-01-06 18:13:12 -0500

    refactor login controller

--- app/assets/javascripts/controllers/login_controller.js
+++ app/assets/javascripts/controllers/login_controller.js
@@ -1,40 +1,48 @@
 Reciclame.LoginController = Ember.Controller.extend({
   init: function(){
-    var currentUser = $USER;
-    console.log(currentUser);
+    var that = this;
+    console.log('currentUser is: ' + currentUser);
 
     navigator.id.watch({
       loggedInUser: currentUser,
-      onlogin: function(assertion) {
-        $.ajax({
-          type: 'POST',
-          url: '/login',
-          beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-          data: {assertion: assertion},
-          success: function(res, status, xhr) {
-            window.location.reload();
-            console.log('persona authentication succesful');
-          },
-          error: function(xhr, status, err) {
-            navigator.id.logout();
-            console.log("Login failure: " + err);
-          }
-        });
-      },
 
-      onlogout: function() {
-        $.ajax({
-          type: 'POST',
-          url: '/logout',
-          beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-          success: function(res, status, xhr) {
-            window.location.reload();
-            console.log('logout from persona succesful');
-          },
-          error: function(xhr, status, err) {
-            console.log("Logout failure: " + err);
-          }
-        });
+      onlogin: that.onLogin(assertion),
+
+      onlogout: that.onLogout
+      }
+    });
+  },
+
+  onLogin: function(assertion){
+    $.ajax({
+      type: 'POST',
+      url: '/login',
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      data: {assertion: assertion},
+      success: function(res, status, xhr) {
+        console.log('persona authentication succesful');
+        console.log(res.email);
+        that.transitionToRoute('places.index');
+      },
+      error: function(xhr, status, err) {
+        console.log("Login failure: " + err);
+        that.transitionToRoute('error')
+      }
+    });
+  },
+
+  onLogin: function() {
+    $.ajax({
+      type: 'POST',
+      url: '/logout',
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      success: function(res, status, xhr) {
+        console.log('logout from persona succesful');
+        that.transitionToRoute('places.index');
+      },
+      error: function(xhr, status, err) {
+        console.log("Logout failure: " + err);
+        that.transitionToRoute('error')
       }
     });
   }

20828f6e532fae63e91eb54faf816f7f8febdd14
Cesar Carruitero <cesar@mozilla.pe>
2014-01-06 18:19:17 -0500

    refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
+
   def create
-    @user = User.authenticate_with_persona(params[:assertion])
-    if @user['email']
-      session[:email] = @user['email']
-      redirect_to root_url
+    audience = 'http://' + request.host_with_port
+    user = User.authenticate_with_persona(params[:assertion], audience)
+    if user['email']
+      session[:email] = user['email']
+      set_current_user session[:email]
+      respond_to do |f|
+        f.json { render json: @current_user }
+      end
     else
       flash.now.alert = 'Invalid email or something'
       redirect_to root_url
@@ -14,4 +19,11 @@ class SessionsController < ApplicationController
     session[:email] = nil
     redirect_to root_url, notice: 'Logged out!'
   end
+
+  private
+
+  def set_current_user email
+    @current_user ||= User.find_or_create_by(email: email)
+  end
+
 end

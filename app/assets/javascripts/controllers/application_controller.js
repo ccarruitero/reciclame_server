ecd596e37b2ea11e4eb69303ba01bc37453b6e7e
ccarruitero <cesar@mozilla.pe>
2015-02-01 22:56:17 -0500

    install app with fx

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -1,2 +1,19 @@
 Reciclame.ApplicationController = Ember.Controller.extend({
+  init: function() {
+    // set install web app with fx
+    if (window.navigator.mozApps !== undefined) {
+      var manifestUrl = 'http://' + window.location.host + '/manifest.webapp';
+      console.log(manifestUrl);
+      var request = window.navigator.mozApps.install(manifestUrl);
+      request.onsuccess = function () {
+        var app = this.result;
+        console.log('app installed');
+      };
+      request.onerror = function() {
+        console.log('error : ' + this.error.name);
+      };
+    }
+    this._super();
+  }
+
 });

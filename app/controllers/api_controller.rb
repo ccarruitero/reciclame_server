3527d66f46bb7d1ed139116f07c8bb17aec79891
ccarruitero <cesar@mozilla.pe>
2015-05-25 09:50:08 -0500

    add cors headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class APIController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :set_cors_headers
   respond_to :json
 
   private
@@ -15,5 +15,12 @@ class APIController < ApplicationController
 
   def respond_api resource
     respond_with :api, resource
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
